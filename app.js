f11466002d0c4960187b9cdb2992cb3b8ec06984
Rafia Qadir <parasramsha@gmail.com>
2020-06-23 16:14:34 +0500

    Updated fies

--- app.js
+++ app.js
@@ -101,8 +101,10 @@
 // var num2=35.36;
 // var integer=Math.floor(num);
 // var integer2=Math.ceil(num2);
-// var string=integer.concat(integer2);
-// document.write(string);
+// var string1=integer.toString();
+// var string2=integer2.toString();
+// var fstring=string1.concat(string2)
+// document.write(fstring);
 
 //13.Write a program to take user input and store username in a variable. 
 // If the username contains any special symbol among [@ . , !], 
@@ -123,7 +125,7 @@
 //  prompt the user whether the given item is found in the list or not. 
 //  Note: Perform case insensitive search. Whether the user enters cookie, Cookie, 
 //  COOKIE or coOkIE, program should inform about its availability. Example: 
- 
+
 // var bakery=prompt("Enter Your item:");
 // bakery=bakery.toLowerCase();
 
@@ -139,7 +141,7 @@
 // }
 
 
-// Write a program to take password as an input from user. 
+// 15.Write a program to take password as an input from user. 
 // The password must qualify these requirements:
 //  a. It should contain alphabets and numbers
 //   b. It should not start with a number
@@ -155,7 +157,7 @@
 // else{
 //   alert("Invalid Password")
 // }
- 
+
 //16. Write a program to convert the following string to an array using string split method. 
 //var university = “University of Karachi”; Display the elements of array in your browser. 
 
@@ -177,7 +179,7 @@
 //  b. round off value of the number 
 //  c. floor value of the number
 //   d. ceil value of the number  
- 
+
 // var integer=prompt("Enter Number ");
 // alert(integer+"\n"+Math.round(integer)+"\n"+Math.floor(integer)+"\n"+Math.ceil(integer));
 
@@ -188,7 +190,7 @@
 //   b. round off value of the number 
 //   c. floor value of the number
 //    d. ceil value of the number 
- 
+
 // var integer=prompt("Enter Negative Number ");
 //  alert(integer+"\n"+Math.round(integer)+"\n"+Math.floor(integer)+"\n"+Math.ceil(integer));
 
@@ -197,7 +199,7 @@
 
 // var num=prompt("Enter number to find absolute value of it");
 // alert("The absoute value of "+num+" "+" is"+" "+Math.abs(num))
- 
+
 //4. Write a program that simulates a dice using random() method of JS Math class.
 // Display the value of dice in your browser.: 
 
@@ -282,7 +284,7 @@
 // alert("Today is"+" "+d);
 
 //4. Write a program that displays a message “It’s Fun day” if its Saturday or Sunday today. 
- 
+
 // var days=["sun","mon","tue","wed","thu","fri","sat"];
 // var today=new Date();
 // var d=days[today.getDay()];
@@ -354,7 +356,7 @@
 // var today=new Date();
 // var get= Math.round(Math.abs((date - startDate) / oneday));
 // alert(get+" "+" Days have passed since 24 April 2020");
- 
+
 //10. Write a program that displays in your browser the seconds that elapsed
 // between the reference date and the beginning of 2015. 
 
@@ -404,7 +406,7 @@
 // var late_pay=350
 //  var net_amount_with_due=unit*charges_p_u;
 //  var gross_a_p=net_amount_with_due+late_pay;
- 
+
 //  alert("Customer Name:"+" "+name+"\n"+
 //  "Month:"+" "+m+"\n"+ "Numbers of unit:"+" "+unit+"\n"+ "Charges per unit:"+" "+charges_p_u+"\n"+ 
 //  "Net amount Payable:"+" "+net_amount_with_due+"\n"+ "Late payment surcharge:"+" "+late_pay+"\n"+ 
@@ -414,36 +416,58 @@
 ///////////////////////////////////////////chapter 35-38///////////////////////////////////////////
 
 //1. Write a function that displays current date & time in your browser. 
-
+// function new_d(){
 // var current=new Date();
-// alert(current)
+// return current
+// }
+// alert(new_d());
 
 //2. Write a function that takes first & last name and then it greets the user using his full name. 
- 
+
 //  function greeting(greet){
-//    var fname="Rafia"
-//    var lname="Memon"
-//    var fullname=fname.concat(lname);
+//    var fname=prompt("Enter firstname")
+//    var lname=prompt("Enter lastname")
+//    return "Hello"+" "+fname.concat(" "+lname);
 //  }
-//  greeting("Hello");
- 
+//  alert(greeting());
+
 //3.. Write a function that adds two numbers (input by user) and returns the sum of two numbers
 
 // function sum(){
-//   var num1=prompt("Enter num 1")
-//   var num2=prompt("Enter num 2");
-//   var sum=num1+num2
-//   return sum;
+//   var num1=+prompt("Enter num 1")
+//   var num2=+prompt("Enter num 2");
+//      var add= num1+num2;
+//      return add
 // }
-// console.log(sum())
+//  console.log(sum())
 
 //4. Calculator:  Write a function that takes three arguments num1, num2 & operator & compute the desired operation.
 // Return and show the desired result in your browser
- 
-// function cal(a,b){
-// return a*b
+
+// function cal(){
+//   var n1=+prompt("Enter first num")
+//   var n2=+prompt("Enter sec num");
+//   var op=prompt("Enter desired opertaion")
+//   if (op=="+"){
+// return n1+n2
+//   }
+//   else if(op=="-"){
+//     return n1-n2
+//   }
+//   else if(op=="*"){
+//     return n1*n2
+//   }
+//   else if(op=="/"){
+//     return n1/n2
+//   }
+//   else if(op=="%"){
+//     return n1%n2
+//   }
+//   else{
+//     return "Wrong operation"
+//   }
 // }
-// alert(cal(4,5));
+// alert(cal());
 
 //5. Write a function that squares its argument. 
 
@@ -457,3 +481,88 @@
 //   return a*b*c*d*e 
 // }
 // alert(fact(5,4,3,2,1));
+
+//7. Write a function that take start and end number as inputs & display counting in your browser. 
+
+//  function counting(){
+//    var start=+prompt("Enter starting value");
+//    var end=+prompt("Enter Ending vaue");
+//    for(var i=start;i<=end;i++){
+//     console.log(i)
+//    }
+//  }
+// counting();
+
+//8. Write a nested function that computes hypotenuse of a right angle triangle. 
+// Hypotenuse2 = Base2 + Perpendicular2 Take base and perpendicular as inputs.
+// Outer function : calculateHypotenuse() Inner function: calculateSquare()
+
+// function hypotenuse(){
+//  var per=+prompt("Enter perpendicular");
+//  var base=+prompt("Enter base ");
+//   var hypo= Math.hypot(per,base)
+//   return hypo
+//   function calsq(){
+//     return hypo*hypo
+
+//   }
+//   //  function square(per,base){
+//   //    return
+//   //   var sq=Math.sqrt(square(per)+square(base));
+//   //   return sq
+//   //  }
+// }
+// alert(hypotenuse()+"\n"+calsq());
+
+//9. Write a function that calculates the area of a rectangle.    
+//   A = width * height     Pass width and height in following manner: 
+// i. Arguments as value ii. Arguments as variables 
+
+//By values
+// function area(width,height){
+//   return width*height
+// }
+// alert(area( "Area of rectangle is "+"  "+5,6.7));
+
+//By variable
+// function area(){
+//   var v1=+prompt("Enter width");
+//   var v2 =+prompt("Enter Height");
+//  return v1*v2
+// }
+// alert(area());
+
+//10. Write a JavaScript function that checks whether a passed string is palindrome or not?  
+//  A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam. 
+
+// function palindrome(){
+//   var str="Madam";
+
+// }
+
+//11. Write a JavaScript function that accepts a string as a parameter and 
+//converts the first letter of each word of the string in upper case.  EXAMPLE STRING :
+ //'the quick brown fox'  EXPECTED OUTPUT : 'The Quick Brown Fox' 
+
+//  function first_letter(){
+//    var str="the quick brown fox".split(' ');
+//    alert("String:"+" "+str+"\n");
+//    for (var i = 0; i < str.length; i++) {
+//        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+//      alert("Expected Output:"+" "+str);
+//                   }
+//  }
+//  first_letter();
+
+//12. Write a JavaScript function that accepts a string as a parameter and 
+//find the longest word within the string.  EXAMPLE STRING : 'Web Development Tutorial'  
+//EXPECTED OUTPUT : 'Development'
+
+// function str(string){
+//   return 
+// }
+
+//13. Write a JavaScript function that accepts two arguments, a string and a 
+//letter and the function will count the number of 
+//occurrences of the specified letter within the string.  Sample arguments : 'JSResourceS.com', 'o'  
+ 
