ad8d38422845e54af41d133733c163a9d78aef1b
Rafia Qadir <parasramsha@gmail.com>
2020-06-23 18:51:38 +0500

    Updated fies

--- app.js
+++ app.js
@@ -577,21 +577,6 @@
 //find the longest word within the string.  EXAMPLE STRING : 'Web Development Tutorial'  
 //EXPECTED OUTPUT : 'Development'
 
-// function find_longest_word(string)
-// {
-//   var array1 = string.match(/\w[a-z]{0,}/gi);
-//   var result = array1[0];
-
-//   for(var i = 1 ; i < array1.length ; i++)
-//   {
-//     if(result.length < array1[i].length)
-//     {
-//     result = array1[i];
-//     } 
-//   }
-//   return result;
-// }
-// console.log(find_longest_word("Web Development Tutorial"));
 
 //  function str(string){
 //   var mess="Web Development Tutorial"
